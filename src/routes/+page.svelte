7baec83e6b7477005281fa2605563e843619c942
Jacob Davis <ismyhc@gmail.com>
2025-04-29 21:40:38 -0400

    meta stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { browser } from '$app/environment';
 	import { onMount } from 'svelte';
-	import { truncateMiddle, formatDate, linkify } from '$lib';
+	import { formatDate, linkify } from '$lib';
 
 	import { SimplePool } from 'nostr-tools/pool';
 	import type { Event, EventTemplate } from 'nostr-tools';
